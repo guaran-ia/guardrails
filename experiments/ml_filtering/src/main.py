5bea4bcbc3a64a8987fa56b795355795b80d6d82
Andrea <andynicolebaezv@gmail.com>
2026-01-22 20:13:20 +0000

    Built pipeline for ML-based filtering

--- experiments/ml_filtering/src/main.py
+++ experiments/ml_filtering/src/main.py
@@ -2,21 +2,24 @@ import os
 from utils import load_bad_words
 from ml_filtering import process_existing_corpora
 
+MODELS_PATHS = ["mmaguero/toxic-multilingual-bert-gn-base-cased", "mmaguero/toxic-gn-bert-tiny-cased"]
+
 def main():
-    current_file = os.path.abspath(__file__)
-    src_dir = os.path.dirname(current_file)
-    main_directory = os.path.dirname(src_dir) # -> experiments/ml_filtering
-    experiments_directory = os.path.dirname(main_directory)
+    current_file = os.path.abspath(__file__) 
+    src_dir = os.path.dirname(current_file) #experiments/ml_filtering/src/
+    ml_filtering_directory = os.path.dirname(src_dir) # -> experiments/ml_filtering/
+    experiments_directory = os.path.dirname(ml_filtering_directory) #experiments/
 
     input_data_directory = os.path.join(experiments_directory, 'data', 'existing_corpora', 'data', 'processed')
     
-    model_path = "mmaguero/toxic-multilingual-bert-gn-base-cased"
-    model_name = model_path.split('/')[-1]
-    output_directory = os.path.join(main_directory, 'data', 'processing_results', model_name)
+    for model_path in MODELS_PATHS:
+        model_path = "mmaguero/toxic-multilingual-bert-gn-base-cased"
+        model_name = model_path.split('/')[-1]
+        output_directory = os.path.join(ml_filtering_directory, 'data', 'processing_results', model_name)
 
-    print(f"Starting Processing with model {model_name}")
+        print(f"Starting Processing with model {model_name}")
 
-    process_existing_corpora(input_data_directory, output_directory, model_path, model_name)
+        process_existing_corpora(input_data_directory, output_directory, model_path, model_name)
 
 
 if __name__ == "__main__":
